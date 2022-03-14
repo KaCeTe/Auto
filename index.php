5b1d6fd92b89e3845e8f67f465d959707cd9fa82
KaCeTe <53702626+KaCeTe@users.noreply.github.com>
2022-03-14 07:28:27 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<title>Hacked by PrasZ7 !</title>
+
+<link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow|Just+Another+Hand" rel="stylesheet"> 
+<center>
+<style>
+@keyframes shake {
+  0% { transform: translate(1px, 1px) rotate(0deg); }
+  10% { transform: translate(-1px, -2px) rotate(-1deg); }
+  20% { transform: translate(-3px, 0px) rotate(1deg); }
+  30% { transform: translate(3px, 2px) rotate(0deg); }
+  40% { transform: translate(1px, -1px) rotate(1deg); }
+  50% { transform: translate(-1px, 2px) rotate(-1deg); }
+  60% { transform: translate(-3px, 1px) rotate(0deg); }
+  70% { transform: translate(3px, 1px) rotate(-1deg); }
+  80% { transform: translate(-1px, -1px) rotate(1deg); }
+  90% { transform: translate(1px, 2px) rotate(0deg); }
+  100% { transform: translate(1px, -2px) rotate(-1deg); }
+}
+.x{
+	  animation: shake 1s;
+  animation-iteration-count: infinite;
+	font-family: 'Just Another Hand', cursive;
+	font-size:58px;
+}
+.z{
+	font-family: 'PT Sans Narrow', cursive;
+	font-size:20px;
+}
+</style>
+<body bgcolor='black'><font color='white'><pre>
+
+</pre>
+<div class=x>Stamped! by PrasZ7 &#x1F494;</div><br>
+<div class=z>
+<br><br><br><br>
+    
+Greetz<br>
+Jatim Blackhat ~ IndoGhostSec ~ Destroy Squad ~ KebumenXcode ~ Zone Xploiter
+</div>
+
